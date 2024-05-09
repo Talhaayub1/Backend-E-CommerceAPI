8d47b7a674bc44d07698ccfcb70a08c29b2bc99e
Talha Ayub <khantalharahi@gmail.com>
2024-05-10 01:09:08 +0500

    tiny change in errors folder

--- errors/index.js
+++ errors/index.js
@@ -1,7 +1,7 @@
 import CustomAPIError from "./custom-api.js";
 import UnauthenticatedError from "./unauthenticated.js";
 import NotFoundError from "./not-found.js";
-import BadRequestError from "./bad-request.js";
+import BadRequestError from "./Bad-Request.js";
 import UnauthorizedError from "./Unatuorized.js";
 export {
   CustomAPIError,
